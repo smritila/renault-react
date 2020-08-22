464a7b06f3fd8a245ee4066eafa13cc4aea51d5c
Alapan Chatterjee <chatterjee.alapan07@gmail.com>
2020-08-22 20:34:38 +0530

    Implement banner component

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,25 +1,25 @@
 import React, { Fragment } from 'react';
 
 import Navbar from '../components/Navigation/Navbar';
+import Banner from '../components/Banner/Banner';
 
 class Layout extends React.Component {
     
-    // constructor(props) {
-    //     super(props);
-    // }
-
     render() {
         return (
             <Fragment>
                 <Navbar />
-                {/* <div>Banner</div>
+                <Banner 
+                    classes={['banner_style_2']} 
+                    backgroundImage="test-drive/test_drive_banner.png"
+                    bannerText="Test Drive" 
+                />
                 <main>
                     { this.props.children }
                 </main>
-                <div>Footer</div> */}
+                <div>Footer</div>
             </Fragment>
         );
-        
     }
 }
 
