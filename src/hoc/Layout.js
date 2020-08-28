21e0e68afc81dc6ac1f0c0c135d450acdb587d15
Alapan Chatterjee <chatterjee.alapan07@gmail.com>
2020-08-28 11:27:58 +0530

    Fix side drawer issue

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,14 +1,33 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import Navbar from '../components/Navigation/Navbar';
 import Footer from '../components/Footer/Footer';
 import Banner from '../components/Banner/Banner';
+import SideDrawer from '../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showSideDrawer: false
+        };
+    }
+
+    sideDrawerToggledHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
     render() {
         return (
             <div className="has_section_bg">
-                <Navbar />
+                <Navbar drawerToggleClicked={this.sideDrawerToggledHandler} />
+                <SideDrawer 
+                    closed={this.sideDrawerToggledHandler} 
+                    isOpen={this.state.showSideDrawer}
+                />
                 <Banner 
                     classes={['banner_style_2']} 
                     backgroundImage="test-drive/test_drive_banner.png"
