84fedd60ab37373cff5f464cd9f45b367f0a591e
Alapan Chatterjee <chatterjee.alapan07@gmail.com>
2020-09-02 11:10:07 +0530

    Implement fullpage slider for captur page

---
+++ src/containers/CarModels/Details/Captur.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FullPageSlider from '../../../components/UI/FullPageSlider/FullPageSlider';
+
+class Captur extends React.Component {
+
+    render() {
+        return <div className="hero">
+            <div className="wrapper">
+                <FullPageSlider />
+            </div>
+        </div>;
+    }
+}
+
+export default Captur;
